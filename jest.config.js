425a445fb3bc9c69dcc88ad138f546cb4f3acacb
ldc <ldc@ldc.com>
2023-03-12 10:56:44 +0800

    [feat]: add jest config

--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,16 @@
+const ignorePath = [
+  "node_modules",
+  "<rootDir>/src/packages/index.ts"
+];
+
 /** @type {import("jest").Config} */
 const config = {
+  rootDir: "./",
   bail: true,
   verbose: true,
-  collectCoverage: true
+  collectCoverage: true,
+  coveragePathIgnorePatterns: ignorePath,
+  testPathIgnorePatterns: ignorePath
 };
 
 export default config;
